70aca68a27645d720bf1d2154594f2572caba14b
Trisha Shetty <shettytrisha2106@gmail.com>
2026-09-03 00:45:12 +0530

    added typeconversion

---
+++ src/typeconversion.java
@@ -0,0 +1,12 @@
+public class typeconversion {
+    public static void main(String[] args) {
+
+        int number = 25;
+
+        // Converting int to double
+        double decimalNumber = number;
+
+        System.out.println("Integer value: " + number);
+        System.out.println("Converted double value: " + decimalNumber);
+    }
+}
