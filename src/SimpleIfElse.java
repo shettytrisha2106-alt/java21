2635453a7ff64d9b6d81942c61667f0b06f0b79c
Trisha Shetty <shettytrisha2106@gmail.com>
2026-09-02 22:50:18 +0530

    Added simpleIf project

---
+++ src/SimpleIfElse.java
@@ -0,0 +1,15 @@
+public class SimpleIfElse {
+
+    public static void main(String[] args) {
+
+        int age = 17;
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
+
+
