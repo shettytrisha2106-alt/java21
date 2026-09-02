4f9d137bc6b1030758669fb1975c3164cf8fabdb
Trisha Shetty <shettytrisha2106@gmail.com>
2026-09-03 00:41:56 +0530

    added Arithmeticoperators

---
+++ src/Arithmeticoperators.java
@@ -0,0 +1,30 @@
+public class Arithmeticoperators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational Operators
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a == b: " + (a == b));
+
+        // Logical Operators
+        System.out.println("(a > b && b > 0): " + (a > b && b > 0));
+
+        // Assignment Operator
+        a += 5;
+        System.out.println("a after += 5: " + a);
+
+        // Unary Operators
+        a++;
+        System.out.println("a after increment: " + a);
+    }
+}
